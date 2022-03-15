d258a893e0dfbeab5794e01d446503ec0cfa0fe7
MASHOD0 <mashhood2002@gmail.com>
2022-03-15 11:47:34 +0530

    add  query

--- src/DB/query.py
+++ src/DB/query.py
@@ -4,3 +4,9 @@ add_new_user="""
 get__pw="""
         SELECT password FROM Users WHERE name = '{}';
         """
+get_history="""
+            SELECT Sentence_history.sentence_id, Sentence_history.sentence, History.search_id, History.time
+            FROM History
+            JOIN Sentence_history ON History.search_id=Sentence.search_id
+            WHERE username = '{}';
+            """
