976ff693c51b4fad1b83fa3c440917e0cf4c9a38
MASHOD0 <mashhood2002@gmail.com>
2022-03-15 21:58:39 +0530

    update

--- src/NLP/nlp.py
+++ src/NLP/nlp.py
@@ -17,11 +17,3 @@ def get_sentences(text, language):
     sentences=sentence_tokenize.sentence_split(indic_string, lang='hi')
     return sentences
     
-
-
-indic_string="""तो क्या विश्व कप 2019 में मैच का बॉस टॉस है? यानी मैच में हार-जीत में \
-टॉस की भूमिका अहम है? आप ऐसा सोच सकते हैं। विश्वकप के अपने-अपने पहले मैच में बुरी तरह हारने वाली एशिया की दो टीमों \
-पाकिस्तान और श्रीलंका के कप्तान ने हालांकि अपने हार के पीछे टॉस की दलील तो नहीं दी, लेकिन यह जरूर कहा था कि वह एक अहम टॉस हार गए थे।"""
-sentences=sentence_tokenize.sentence_split(indic_string, lang='hi')
-for t in sentences:
-    print(t)
