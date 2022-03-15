2614a9fd62cac6711575f155d796821a5266b6ab
MASHOD0 <mashhood2002@gmail.com>
2022-03-15 11:47:14 +0530

    add  query

--- src/DB/query.py
+++ src/DB/query.py
@@ -1,3 +1,6 @@
 add_new_user="""
             INSERT INTO Users (username, password) VALUES ('{}', '{}');
             """
+get__pw="""
+        SELECT password FROM Users WHERE name = '{}';
+        """
