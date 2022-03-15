ef9e7f6e2d19ff4172f82842f17ea3230ae9393e
MASHOD0 <mashhood2002@gmail.com>
2022-03-15 21:57:18 +0530

    bug fix

--- src/DB/query.py
+++ src/DB/query.py
@@ -1,7 +1,7 @@
 add_new_user="""
             INSERT INTO Users (username, password) VALUES ('{}', '{}');
             """
-get__pw="""
+get_pw="""
         SELECT password FROM Users WHERE name = '{}';
         """
 get_history="""
@@ -10,3 +10,9 @@ get_history="""
             JOIN Sentence_history ON History.search_id=Sentence.search_id
             WHERE username = '{}';
             """
+get_all_data="""
+                SELECT Articles.article_id, Articles.link, Articles.metadata, Article_sentence.sentence_id, Article_sentence.sentence
+                FROM Articles
+                JOIN Article_sentence ON Articles.article_id=Article_sentence.article_id
+                WHERE Articles.language = (SELECT language_id FROM Languages WHERE language = '{}');
+                """
