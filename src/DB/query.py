872065e41e61473c91b7d42a724804686e9683f7
MASHOD0 <mashhood2002@gmail.com>
2022-03-16 23:18:47 +0530

    add new queries and correct old queries

--- src/DB/query.py
+++ src/DB/query.py
@@ -16,6 +16,14 @@ get_all_data="""
                 JOIN Article_sentence ON Articles.article_id=Article_sentence.article_id
                 WHERE Articles.language = (SELECT language_id FROM Languages WHERE language = '{}');
                 """
-add_history=""""""
-get_search_id=""""""
-add_sentence_history=""""""
+add_history="""INSERT INTO public."History"( id, "time", language)
+                VALUES ((SELECT id from "Users" where username='{}'), TIMESTAMP '{}', '{}');
+                """
+get_search_id="""SELECT search_id FROM "History" WHERE "time" = '{}' AND id = (select "id" from "Users" where username= '{}');""" 
+add_sentence_history="""INSERT INTO public."Sentence_history"( sentence, search_id)
+	                VALUES ( '{}', {});"""
+
+get_history= """SELECT "History".search_id, "History".language, "History".time, "Sentence_history".sentence_id, "Sentence_history".sentence
+                FROM "History"
+                JOIN "Sentence_history" ON "History".search_id = "Sentence_history".search_id
+                WHERE "History".id = (SELECT "id" FROM "Users" WHERE username = '{}')"""
