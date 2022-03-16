dccc7453d563470d2b64408a501d275d04e2a77c
MASHOD0 <mashhood2002@gmail.com>
2022-03-17 00:55:45 +0530

    bugfix: indic_string bug

--- src/NLP/nlp.py
+++ src/NLP/nlp.py
@@ -14,6 +14,6 @@ def get_sentences(text, language):
     
     common.set_resources_path(INDIC_NLP_RESOURCES)
     loader.load()
-    sentences=sentence_tokenize.sentence_split(indic_string, lang='hi')
+    sentences=sentence_tokenize.sentence_split(text, lang='hi')
     return sentences
     
