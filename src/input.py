5a959585d263755e8052eae7636d10f9bbda854f
MASHOD0 <mashhood2002@gmail.com>
2022-03-17 00:54:55 +0530

    add add_to_db()

--- src/input.py
+++ src/input.py
@@ -8,4 +8,18 @@ def input_data(language: str, text: str):
     print(len(sentences))
     return sentences
 
+def add_to_db(conn, language, link, metadata, sentences):
+    lang = db.fetch(conn, q.lang)
+    for i in lang:
+        if i[1] == language:
+            language_id = i[0]
+        
+
+    db.execute(conn, q.add_article.format(language_id, link, metadata))
+    article_id = db.fetch(conn, q.get_article_id.format(link))[0][0]
+    for sentence in sentences:
+        print(sentence)
+        db.execute(conn, q.add_sentence.format(article_id, sentence))
+    print("Added to DB")
+    
 
