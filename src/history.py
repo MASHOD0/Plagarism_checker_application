afdb3af9c0aa8f409a6086951cd68945b53d6aa2
MASHOD0 <mashhood2002@gmail.com>
2022-03-16 23:17:58 +0530

    add queries

--- src/history.py
+++ src/history.py
@@ -1,7 +1,12 @@
 from DB import db, query as q
 
-def create_history(conn, username, timestamp, sentences):
-    db.execute(conn, q.add_history.format(username, timestamp))
-    search_id=db.fetch(conn, q.get_search_id.format(username, timestamp))[0]
+def create_history(conn, username, timestamp, sentences, language):
+    db.execute(conn, q.add_history.format(username, timestamp, language))
+    search_id=db.fetch(conn, q.get_search_id.format(timestamp, username))[0][0]
     for sentence in sentences:
-        db.execute(conn, q.add_sentence_history.format(search_id, sentence))
+        db.execute(conn, q.add_sentence_history.format(sentence, search_id))
+
+
+def get_history(conn, username):
+    history = db.fetch(conn, q.get_history.format(username))
+    return history
