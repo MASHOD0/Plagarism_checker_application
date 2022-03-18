171d7ee3e2f2295c655684a3fe0a96b3206600e8
MASHOD0 <mashhood2002@gmail.com>
2022-03-18 16:03:14 +0530

    feat: generate_report()

--- src/report.py
+++ src/report.py
@@ -10,11 +10,12 @@ def fetch_data(conn, language: str):
 
 def generate_report(sentences: list, language: str):
     data = fetch_data(conn, language)
+    # data format: article_id, link, metadata, sentence_id, sentence
     # p_words = 0
-    p_articles = []
+    p_links = []
     score = 0.0
     highest_score = 0.0
-    report = []
+    p_articles = []
     lable = []
     temp_article_sentence = []
     for sentence in sentences:
@@ -23,19 +24,26 @@ def generate_report(sentences: list, language: str):
             if plagarism > highest_score:
                 highest_score = plagarism
                 temp_article_sentence = i
-        if highest_score > 0.75 and highest_score < 0.85:
+        if highest_score > 0.75 and highest_score <= 0.85:
             p_articles.append(temp_article_sentence)
             lable.append("Related Meaning")
             score += highest_score
-        elif highest_score > 0.85:
+           
+        elif highest_score > 0.85 and highest_score <= 0.95:
+            p_articles.append(temp_article_sentence)
+            lable.append("Minor changes")
+            score += highest_score
+        elif highest_score > 0.95:
             p_articles.append(temp_article_sentence)
             lable.append("Plagarism")
             score += highest_score
-    
         else :
             lable.append("Not Related")
             score += highest_score
                 
     score = (score/len(sentences))*100
+    for article in p_articles:
+        if article[1] not in p_articles:
+            p_links.append(article)
         
-    return lable, p_words, p_articles, score
+    return lable, p_links, score
